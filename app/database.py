3476af34aa8b4ff24c26546c4a2d27ccad9e7103
Max Dupley <51045274+QuadDarv1ne@users.noreply.github.com>
2024-12-26 18:33:30 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,8 +3,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import DATABASE_URL
 
-# Создаем движок для подключения к базе данных
+# Создаем движок для подключения к базе данных в зависимости от СУБД
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})  # Для SQLite
+
+'''
+Для PostgreSQL, MySQL, SQL Server и других СУБД:
+- PostgreSQL:
+  engine = create_engine("postgresql://username:password@localhost/dbname")
+- MySQL:
+  engine = create_engine("mysql+pymysql://username:password@localhost/dbname")
+- SQL Server:
+  engine = create_engine("mssql+pyodbc://username:password@localhost/dbname?driver=ODBC+Driver+17+for+SQL+Server")
+- MongoDB:
+  Для MongoDB используйте PyMongo, так как это не реляционная база данных.
+'''
 
 # Базовый класс для всех моделей
 Base = declarative_base()
